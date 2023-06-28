40b793ac802c06a58b2873d407f3b920279d3529
bgcuv <117119505+bgcuv@users.noreply.github.com>
2023-06-28 08:47:29 -0700

    added pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,24 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      html: {
+        height: "100%",
+      },
+      body: {
+        height: "100%",
+      },
+      "#__next": {
+        height: "100%",
+      },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
