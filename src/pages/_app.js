7092446b2a16e367a78b4e054adfa240f31a85ff
bgcuv <117119505+bgcuv@users.noreply.github.com>
2023-06-28 09:48:21 -0700

    Updated font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,19 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource/montserrat";
 
 const theme = extendTheme({
+  components: {
+    Heading: {
+      baseStyle: {
+        fontFamily: "Montserrat",
+      },
+    },
+  },
   styles: {
     global: {
-      html: {
+      "html, body": {
         height: "100%",
-      },
-      body: {
-        height: "100%",
+        fontFamily: "Montserrat",
       },
       "#__next": {
         height: "100%",
