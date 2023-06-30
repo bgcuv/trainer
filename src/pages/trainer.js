fc52c9c38f32f2bb5f4733470a70ebb47eb88365
bgcuv <117119505+bgcuv@users.noreply.github.com>
2023-06-30 09:40:56 -0700

    Update trainer.js

--- src/pages/trainer.js
+++ src/pages/trainer.js
@@ -1,6 +1,37 @@
+import { useState, useEffect } from "react";
+import { Center, Flex, Heading } from "@chakra-ui/react";
 import Head from "next/head";
 import Nav from "@/components/Nav";
+
+// moves: Forward Smash, Down Special,
+// Forward Air
+// arrows
+// →⇉
+const moves = [
+  {
+    buttons: "⇉ A",
+    name: "Forward Smash",
+  },
+  {
+    buttons: "⇉ B",
+    name: "Side Special",
+  },
+  {
+    buttons: "ZR or ZL",
+    name: "Shield",
+  },
+  {
+    buttons: "⇈ →",
+    name: "Forward Air",
+  },
+  {
+    buttons: "→",
+    name: "Forward Tilt",
+  },
+];
+
 export default function Home() {
+  const [move, setMove] = useState("jkd");
   return (
     <>
       <Head>
@@ -9,7 +40,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <Flex
+        direction="column"
+        gap={8}
+        p={4}
+        color="whiteAlpha.900"
+        as="main"
+        height="100%"
+        bgGradient="linear(#191970, #9967A4, #C46E9D, #F48D86, #FEBC64, #FFDD5A)"
+      >
+        <Center>
+          <Heading>Trainer</Heading>
+        </Center>
+        {move}
+      </Flex>
       <Nav left="tier-list" right="about" />
     </>
   );
